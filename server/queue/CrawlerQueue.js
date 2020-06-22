059563eb693ad7f287b25a3be7c0a3a579e5b9be
xraybrain <xraybrain@gmail.com>
2020-06-22 11:14:50 +0100

    tracking bull error on server

--- server/queue/CrawlerQueue.js
+++ server/queue/CrawlerQueue.js
@@ -1,15 +1,15 @@
 const Bull = require('bull');
-let RedisHost = '127.0.0.1';
-let RedisPort = 6379;
+let RedisHost = process.env.REDIS_URL || '127.0.0.1';
+let RedisPort = process.env.REDIS_PORT || 6379;
 const { crawler, filterResult } = require('../src/crawler');
 
-const extractPort = (url) => url.substr(url.lastIndexOf(':') + 1);
-const extractUrl = (url) => url.substr(0, url.lastIndexOf(':'));
+// const extractPort = (url) => url.substr(url.lastIndexOf(':') + 1);
+// const extractUrl = (url) => url.substr(0, url.lastIndexOf(':'));
 
-if (process.env.REDIS_URL) {
-  RedisHost = extractUrl(process.env.REDIS_URL);
-  RedisPort = extractPort(process.env.REDIS_URL);
-}
+// if (process.env.REDIS_URL) {
+//   RedisHost = extractUrl(process.env.REDIS_URL);
+//   RedisPort = extractPort(process.env.REDIS_URL);
+// }
 
 console.log(RedisHost, RedisPort);
 
